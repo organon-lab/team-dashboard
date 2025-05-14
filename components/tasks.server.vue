ff33d69d71f6ff1f8e8056f8c06177ba0a36c18b
Yohh <durandyohan@zaclys.net>
2025-05-14 23:25:19 +0200

    task: create task component

---
+++ components/tasks.server.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const prisma = usePrismaClient();
+
+const users = await prisma.user.findMany({
+  include: {
+    tasks: true,
+  },
+});
+
+const tasks = await prisma.task.findMany({
+  include: {
+    user: true,
+  },
+});
+</script>
+
+<template>
+  <div>
+    <h1
+      class="text-2xl font-bold text-center text-red-400"
+    >Tasks</h1>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+	<span class="text-green-400">User: {{ task.user?.name }}</span>
+	&nbsp;
+        <span class="text-green-400">Task: {{ task.title }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
